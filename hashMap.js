5701d028fee7fa5afb80ebb071e38fa4b0d5e081
Ethan <ethancredo.ascc@outlook.com>
2026-05-22 16:40:08 +0800

    initialize

---
+++ hashMap.js
@@ -0,0 +1,19 @@
+class HashMap {
+    constructor() {
+        this.loadFactor = 0.75;
+        this.capacity = 16;
+    }
+
+
+}
+
+function hash(key) {
+    let hashCode = 0;
+
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+        hashCode = primeNumber * hashCode + key.charCodeAt(i);
+    }
+
+    return hashCode;
+}
