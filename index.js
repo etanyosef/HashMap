851b4e2e279b36cf5051f7ef5ebcd79eed247d78
Ethan <ethancredo.ascc@outlook.com>
2026-05-28 09:24:44 +0800

    test hash map

---
+++ index.js
@@ -0,0 +1,8 @@
+import { HashMap } from "./hashMap.js";
+
+const test = new HashMap();
+
+console.log(test.set('gasi', 'cat'))
+console.log(test.set('mimoy', 'aw'))
+console.log(test.set('mimoy', 'cat'))
+console.log(test.table);
