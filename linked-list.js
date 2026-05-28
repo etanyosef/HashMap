c52d1b58813ac366109b1c58814ea7f813f4553d
Ethan <ethancredo.ascc@outlook.com>
2026-05-28 09:59:15 +0800

    create LinkedList class with append method

--- linked-list.js
+++ linked-list.js
@@ -5,3 +5,23 @@ class Node {
     }
 }
 
+class LinkedList {
+    constructor(head = null) {
+        this.head = head;
+    }
+
+    append(value) {
+        const newNode = new Node(value);
+
+        if (!this.head) {
+            this.head = newNode;
+            return;
+        }
+
+        let current = this.head;
+        while (current.nextNode) {
+            current = current.nextNode;
+        }
+        current.nextNode = newNode;
+    }
+}
