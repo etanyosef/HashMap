ad60b67e023c3fbbf1bca8209df0dedd13bf5be9
Ethan <ethancredo.ascc@outlook.com>
2026-05-28 09:24:15 +0800

    add capacity arg and table array in constructor

--- hashMap.js
+++ hashMap.js
@@ -1,19 +1,36 @@
-class HashMap {
-    constructor() {
+export class HashMap {
+    constructor(capacity = 16) {
         this.loadFactor = 0.75;
-        this.capacity = 16;
+        this.capacity = capacity;
+        this.table = new Array(capacity);
     }
 
+    hash(key) {
+        let hashCode = 0;
 
-}
+        const primeNumber = 31;
+        for (let i = 0; i < key.length; i ++) {
+            hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.capacity;
+        }
 
-function hash(key) {
-    let hashCode = 0;
-
-    const primeNumber = 31;
-    for (let i = 0; i < key.length; i++) {
-        hashCode = primeNumber * hashCode + key.charCodeAt(i);
+        return hashCode;
     }
 
-    return hashCode;
+    set(key, value) {
+        const index = this.hash(key);
+
+        if (!this.table[index]) {
+            this.table[index] = [];
+        }
+
+        this.table[index] = value;
+
+        return index;
+    }
+
+    get(key) {
+        const index = this.hash(key);
+        
+    }
+
 }
