fe598b12bd548c329693a712067ada3626f54afe
Ethan Joseph <ethancredo.ascc@outlook.com>
2026-05-31 22:19:46 +0800

    test hashmap

--- index.js
+++ index.js
@@ -5,4 +5,5 @@ const test = new HashMap();
 console.log(test.set('gasi', 'cat'))
 console.log(test.set('mimoy', 'aw'))
 console.log(test.set('mimoy', 'cat'))
-console.log(test.table);
+console.log(test.buckets);
+console.log(test.buckets[11]);
