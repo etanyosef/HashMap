a35b087be49b78c58d0cb320be886dccef20aa68
Ethan Joseph <ethancredo.ascc@outlook.com>
2026-05-31 22:25:28 +0800

    remove space

--- hash-map.js
+++ hash-map.js
@@ -10,8 +10,8 @@ export class HashMap {
 
     hash(key) {
         let hashCode = 0;
-
         const primeNumber = 31;
+        
         for (let i = 0; i < key.length; i ++) {
             hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.capacity;
         }
