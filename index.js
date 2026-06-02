e0553d4faec658023577873ddb18004777a6b325
Ethan <ethancredo.ascc@outlook.com>
2026-06-02 15:26:17 +0800

    test has method

--- index.js
+++ index.js
@@ -8,4 +8,10 @@ console.log(test.set('mimoy', 'cat'))
 console.log(test.set('mimoy', 'aw'))
 console.log(test.buckets);
 console.log(test.buckets[11]);
+
 console.log(test.get('mimoy'));
+console.log(test.get('sagi'));
+
+console.log(test.has('mimoy'));
+console.log(test.has('sagi'));
+console.log(test.has('sagiasd'));
