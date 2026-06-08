49208206a96b800b1160b2460b77b6868fa8ab6b
Ethan <ethancredo.ascc@outlook.com>
2026-06-08 15:25:06 +0800

    initialize hashset

---
+++ hash-set.js
@@ -0,0 +1,32 @@
+import { LinkedList } from "./linked-list.js";
+
+class HashSet {
+    constructor(capacity = 16) {
+        this.capacity = capacity;
+        this.loadFactor = 0.75;
+        this.threshold = this.capacity * this.loadFactor;
+        this.buckets = new Array(capacity);
+    }
+
+    hash(key) {
+        let hashCode = 0;
+        const primeNumber = 31;
+
+        for (let i = 0; i < key.length; i++) {
+            hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.capacity;
+        }
+
+        return hashCode;
+    }
+
+    set(key) {
+        const index = this.hash(key);
+
+        if (!this.buckets[index]) {
+            this.buckets[index] = new LinkedList();
+            this.buckets[index].append(key);
+        } else {
+            this.buckets[index].append(key);
+        }
+    }
+}
